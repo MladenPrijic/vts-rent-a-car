a04dc6ea44de698b5ebbe05219803079e58cda4f
vedrankk <vedrankorponajic@gmail.com>
2016-06-21 19:33:02 +0200

    Dodao jos funkcija u db klasu

--- php_includes/db_class.php
+++ php_includes/db_class.php
@@ -4,6 +4,7 @@ class DB extends mysqli{
 	public function __construct($host,$user,$pass,$db){
 		$this->db= new mysqli($host,$user,$pass,$db);
 	}
+	//GLAVNE FUNKCIJE
 	public function q($sql){
 		return mysqli_query($this->db,$sql);
 	}
@@ -16,12 +17,37 @@ class DB extends mysqli{
 		 return $result;
 		
 	}
+	
+	public function update($table,$set,$where){
+		if(empty($table)){return false;}
+		if(empty($set)){return false;}
+		if(empty($where)){return false;}
+		$set=$this->set($set);
+		$where=$this->where($where);
+		$sql="UPDATE $table SET $set WHERE $where";
+		return $sql;
+		$result=$this->q($sql);
+		return $result;
+	}
+
+	public function insert($table,$field,$val){
+		if(empty($table)){return false;}
+		if(empty($field)){return false;}
+		if(empty($val)){return false;}
+		$field=$this->insertField($field);
+		$val=$this->insertVals($val);
+		$sql="INSERT INTO $table($field) VALUES ($val)";
+		return $sql;
+
+	}
+	//POMOCNE FUNKCIJE
+
 	public function where($where){
 		if(is_array($where)){
 			$data="";
 			foreach($where as $key=>$val){
 				if(!empty($val) || is_numeric($val)){
-				$data.=$key ."=" .$val ." AND ";
+				$data.=$key ."='" .$val ."' AND ";
 			}
 			}
 			$data=substr($data, 0, -4);
@@ -32,14 +58,71 @@ class DB extends mysqli{
 		
 		return $data;
 	}
-}
-$ar=['rented'=>0,'brand'=>'Mercedes'];
-$base=new DB("sql310.freesubdomain.org","frsb_16959767","vlado107","frsb_16959767_rentacar");
+
+	public function set($set){
+		if(is_array($set)){
+			$data="";
+			foreach($set as $key=>$val){
+				if(!empty($val) || is_numeric($val)){
+				$data.=$key ."='" .$val ."', ";
+			}
+			}
+			$data=substr($data, 0, -2);
+		}
+		else{
+			return $set;
+		}
+		return $data;
+	}
+
+	public function insertField($fields){
+		if(is_array($fields)){
+			$data="";
+			foreach($fields as $key=>$val){
+				$data.="'" .$key ."', ";
+			}
+			$data=substr($data, 0, -2);
+		}
+		else{
+			return $fields;
+		}
+		return $data;
+	}
+	public function insertVals($vals){
+		if(is_array($vals)){
+			$data="";
+			foreach($vals as $key=>$val){
+				if(is_string($val)){
+				$data.="'" .$val ."', ";
+			}
+			else{
+				$data.=$val .",";
+
+			}
+			}
+			$data=substr($data, 0, -2);
+		}
+		else{
+			return $vals;
+		}
+		return $data;
+
+	}
+
+	}
+
+
+ $ar=['rented'=>0,'brand'=>'Zastava'];
+ $base=new DB("localhost","vekki","","rent-a-car");
 //echo $base->where($ar);
-$result=$base->select("*","car",$ar,"");
+//$result=$base->select("*","car",$ar,"");
 //echo $result;
-while($row=mysqli_fetch_array($result)){
-	echo $row["brand"] ."<br>";
-	echo $row["model"] ."<br>";
-}
+// while($row=mysqli_fetch_array($result)){
+// 	echo $row["brand"] ."<br>";
+// 	echo $row["model"] ."<br>";
+// }
+ echo $base->insert("car",$ar,$ar);
+
+
+ 
 
